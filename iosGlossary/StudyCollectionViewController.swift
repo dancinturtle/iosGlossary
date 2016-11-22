231827769b8e020fba5cf6dc8463ca9979c8b988
dancinturtle <amy.giver@gmail.com>
2016-11-21 16:49:21 -0800

    flipping works

--- iosGlossary/StudyCollectionViewController.swift
+++ iosGlossary/StudyCollectionViewController.swift
@@ -27,60 +27,34 @@ class StudyCollectionViewController: UICollectionViewController {
     }
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "flashcardCell", for: indexPath as IndexPath) as! CollectionViewCustomCell
-        cell.backgroundColor = UIColor.red
+        
         cell.flashcardLabel.text = lettersarray[indexPath.item]
         return cell
         
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-//        return self.collectionView!.bounds.size;
-        return CGSize(width: view.bounds.width, height: view.bounds.height/1.15)
+
+        return CGSize(width: view.bounds.width, height: view.bounds.height/1.1)
         
     }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets{
-//        let leftRightInset = self.view.frame.size.width / 14.0
-//        return UIEdgeInsetsMake(0, leftRightInset, 0, leftRightInset)
-//    }
-    
+
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator){
         super.viewWillTransition(to: size, with: coordinator)
-        print("Screen transition")
-//        indexPathsForVisibleItems = collectionView?.indexPathsForVisibleItems
-//        print("Index Paths", indexPathsForVisibleItems)
-        let viewPoint = CGPoint(x: (self.collectionView?.center.x)!, y: (self.collectionView?.center.y)!)
-        indexPathOnRotation = self.collectionView?.indexPathForItem(at: viewPoint) as NSIndexPath?
-        print("Got a new index path", indexPathOnRotation)
-        updateCollectionViewLayout(with: size)
-        
+        let offset = collectionView?.contentOffset
+        let width = collectionView?.bounds.size.width
+        let index = round(offset!.x / width!)
+        let newOffset = CGPoint(x: index * size.width, y: offset!.y)
+        collectionView?.setContentOffset(newOffset, animated: false)
+        coordinator.animate(alongsideTransition: {(context) in self.collectionView?.reloadData(); self.collectionView?.setContentOffset(newOffset, animated: false)}, completion: nil)
     }
-    
-    private func findCenterIndex(){
-        let center = self.view.convert(self.collectionView.center, to: self.collectionView)
-        let index = collectionView!.indexPathForItem(at: center)
-        print(index)
-    }
+
     
 
 
     private func updateCollectionViewLayout(with size: CGSize){
         
         if let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout {
-            layout.itemSize = (size.width < size.height) ? CGSize(width: view.bounds.width, height: view.bounds.height/1.15) : CGSize(width: view.bounds.width, height: view.bounds.height/1.15)
-            
-            if let scrollTo = indexPathOnRotation {
-                collectionView?.scrollToItem(at: scrollTo as IndexPath, at: UICollectionViewScrollPosition.centeredHorizontally, animated: false)
-            }
-//            if let cellIndexPath: NSIndexPath = self.collectionView?.indexPathForItem(at: viewPoint!) as NSIndexPath? {
-//                print ("scroll to here", cellIndexPath)
-//                collectionView?.scrollToItem(at: cellIndexPath as IndexPath, at: UICollectionViewScrollPosition.centeredHorizontally, animated: false)
-//
-//            }
-            
-//            if let scrollTo = indexPathsForVisibleItems {
-//                print("Scrollin to", scrollTo[0])
-//                collectionView?.scrollToItem(at: scrollTo[0], at: UICollectionViewScrollPosition.centeredHorizontally, animated: false)
-//                
-//            }
+            layout.itemSize = (size.width < size.height) ? CGSize(width: view.bounds.width, height: view.bounds.height/1.1) : CGSize(width: view.bounds.width, height: view.bounds.height/1.1)
             
             layout.invalidateLayout()
         }
@@ -88,9 +62,5 @@ class StudyCollectionViewController: UICollectionViewController {
         
         
     }
-    
-//    override func targetContentOffsetForProposedContentOffset(proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-//        
-//    }
 
 }
